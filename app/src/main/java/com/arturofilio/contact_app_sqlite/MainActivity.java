a9624562861e8952c22b6e909020ce694c8156d1
Arturo Filio Villa <arturfil@hotmail.com>
2018-06-28 01:24:00 -0500

    setup for the list layout

--- app/src/main/java/com/arturofilio/contact_app_sqlite/MainActivity.java
+++ app/src/main/java/com/arturofilio/contact_app_sqlite/MainActivity.java
@@ -1,13 +1,35 @@
 package com.arturofilio.contact_app_sqlite;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started");
+
+        init();
     }
+
+    /**
+     *  initialize the first fragment (ViewContactsFragment)
+     */
+    private void init() {
+        ViewContactsFragment fragment = new ViewContactsFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        // replace whtaerver is in the fragment container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/arturofilio/contact_app_sqlite/ViewContactsFragment.java
@@ -0,0 +1,42 @@
+package com.arturofilio.contact_app_sqlite;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ViewContactsFragment extends Fragment {
+
+    private static final String TAG = "ViewContactsFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_viewcontacts, container, false);
+        Log.d(TAG, "onCreateView: started");
+
+        // navigate to add contacts fragment
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fabAddContact);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked fab");
+            }
+        });
+
+        ImageView ivSearchContact = (ImageView) view.findViewById(R.id.ivSearchIcon);
+        ivSearchContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked search icon.");
+            }
+        });
+
+        return view;
+    }
+}
