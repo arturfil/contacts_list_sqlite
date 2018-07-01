1aabd46decf3801f2b89a4d706b19e78ced9ba4e
Arturo Filio Villa <arturfil@hotmail.com>
2018-07-01 01:52:06 -0500

    added the universal image loader

---
+++ app/src/main/java/com/arturofilio/contact_app_sqlite/models/Contact.java
@@ -0,0 +1,58 @@
+package com.arturofilio.contact_app_sqlite.models;
+
+public class Contact {
+
+    private String name;
+    private String phonenumber;
+    private String device;
+    private String email;
+    private String profileImage;
+
+    public Contact(String name, String phonenumber, String device, String email, String profileImage) {
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.device = device;
+        this.email = email;
+        this.profileImage = profileImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+}
